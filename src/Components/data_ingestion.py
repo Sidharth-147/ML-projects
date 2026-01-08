f6440755f9effa4b09ca9c8b5ed50de5e5bde4bf
Sidharth-147 <saisidharth288@gmail.com>
2026-01-08 21:42:43 +0100

    Data Ingestion

---
+++ src/Components/data_ingestion.py
@@ -0,0 +1,27 @@
+import os
+from src import DataIngestion
+
+from dataclasses import dataclass
+
+from src.Components.data_transformation import DataTransformation
+from src.Components.data_transformation import DataTransformationConfig
+
+from src.Components.model_trainer import ModelTrainerConfig
+from src.Components.model_trainer import ModelTrainer
+@dataclass
+class DataIngestionConfig:
+    train_data_path: str=os.path.join('artifacts',"train.csv")
+    test_data_path: str=os.path.join('artifacts',"test.csv")
+    raw_data_path: str=os.path.join('artifacts',"data.csv")
+
+if __name__ == "__main__":
+    obj=DataIngestion()
+    train_data,test_data=obj.initiate_data_ingestion()
+
+    data_transformation=DataTransformation()
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
+
+    modeltrainer=ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
+
+
