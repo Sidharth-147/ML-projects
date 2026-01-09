68003d22c10ed39edcf15ed50b6a32e56837d7fc
Sidharth-147 <saisidharth288@gmail.com>
2026-01-09 15:46:36 +0100

    Hyperparameter Training

--- src/utils.py
+++ src/utils.py
@@ -22,7 +22,7 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e, sys)
     
-def evaluate_models(X_train, y_train, X_test, y_test, models):
+def evaluate_models(X_train, y_train, X_test, y_test, models,param):
      try:
         
         report = {}
@@ -38,7 +38,6 @@ def evaluate_models(X_train, y_train, X_test, y_test, models):
             y_test_pred = model.predict(X_test)
 
             train_model_score = r2_score(y_train, y_train_pred)
-
             test_model_score = r2_score(y_test, y_test_pred)
 
             report[list(models.keys())[i]] = test_model_score
